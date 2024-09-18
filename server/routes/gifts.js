9fc8ca9e25f5d49841e7ba04ac9dcd3e3591b009
Jasmine Yue <yue.x@northeastern.edu>
2024-09-18 15:59:30 -0700

    lab1 for Jasmine

---
+++ server/routes/gifts.js
@@ -0,0 +1,30 @@
+import express from "express";
+
+import giftData from "../data/gifts.js";
+
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log("__filename: ", __filename);
+console.log("__dirname: ", __dirname);
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.status(200).json(giftData);
+});
+
+/* router.get("/:giftId", (req, res) => {
+  res
+    .status(200)
+    .json(giftData.find((gift) => gift["id"] === parseInt(req.params.giftId)));
+}); */
+
+router.get("/:giftId", (req, res) => {
+  res.status(200).sendFile(path.resolve(__dirname, "../public/gift.html"));
+});
+
+export default router;
